81c5f8ee4e338ea20f6ddfb782f7e01867439680
Leandro Saraiva <leandross91@gmail.com>
2023-07-31 18:21:05 -0300

    set remove dist src js on dist

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from "vite-plugin-svgr";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), svgr()],
+  base: "/rocketshoes",
   resolve: {
     alias: [
       { find: '@', replacement: path.resolve(__dirname, 'src') },
