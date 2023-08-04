78c9849d24e07b604b169e27f7b0d657123aa142
Leandro Saraiva <leandross91@gmail.com>
2023-08-04 17:46:00 -0300

    dist

--- src/components/elements/Logo/styles.ts
+++ src/components/elements/Logo/styles.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components'
 import { ReactComponent as LogoTextSVG } from '@/assets/logo/ROCKETSHOES.svg'
 import { ReactComponent as LogoSneckerSVG } from '@/assets/logo/noun_Running_1473810.svg'
+import { metrics } from '@/styles/metrics'
 
 export const Container = styled.div``
 export const LogoText = styled(LogoTextSVG)``
